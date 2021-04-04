2dfee6bf5bc69c594f072af4909ee557d7e6cb12
JainRita <ritajain11969@gmail.com>
2021-04-04 10:49:41 +0530

    First Vue Commit

---
+++ src/components/BookForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
+    <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <input type="checkbox" v-model="bookData.finishedReading"> <label for="finishedReading"> Finished Reading </label>
+    <input type="radio" v-model="bookData.ownership" value="borrowed"> <label for="borrowed"> Borrowed </label>
+    <input type="radio" v-model="bookData.ownership" value="bought"> <label for="bought"> Bought </label>
+    <button type="submit" name="button" v-on:submit.prevent="bookSubmit()">Add Book</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "BookForm",
+  props: ["books"],
+  data() {
+    return {
+      bookData: {
+        bookTitle: "",
+        bookAuthor: "",
+        finishedReading: false,
+        ownership: []
+      }
+    };
+  },
+  methods: {
+    bookSubmit(bookData) {
+      this.$emit("addBook", bookData);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
